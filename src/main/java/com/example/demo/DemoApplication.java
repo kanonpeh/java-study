27fd66e3c15160bf7ed65f141dcaf596c67a0a7b
kanon <kanonpeh@gmail,com>
2025-10-03 01:02:58 +0900

    add initial DemoApplication with greeting messages in Japanese

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class DemoApplication {
+
+    public static void main(String[] args) {
+    System.out.println("こんにちはみのろコーチです。");
+    System.out.print("こんにちは");
+    System.out.println("速水剛瑠です。");
+    System.out.println("こんにちは。河合瑠璃です。");
+}
+
+
+}
