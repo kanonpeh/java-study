e33fa84f80b80ef657ebd4e26f82ab267fdd69db
kanon <kanonpeh@gmail,com>
2025-10-04 01:04:13 +0900

    add Variable class with age example in Japanese

---
+++ src/main/java/com/example/demo/Variable.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+/*
+*変数のサンプル１
+ */
+public class Variable {
+    public static void main(String[] args) {
+
+        int age;        //変巣の宣言
+       age = 20; //変数への値の代入
+
+        //剛留くん情報の表示を
+        System.out.println("剛留くん:年齢は" + age + "歳です。");
+    }
+}
