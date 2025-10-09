c36d7f05f677463b4d130d713fb2fa84d1f55b05
kanon <kanonpeh@gmail,com>
2025-10-09 21:35:15 +0900

    add SelfIntroduction class with self-introduction example in Japanese

---
+++ src/main/java/com/example/demo/SelfIntroduction.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+public class SelfIntroduction {
+    public static void main(String[] args) {
+        // 自己紹介を表示する
+        System.out.print("こんにちは。○○○○です。");
+        System.out.println("△△県出身です。");
+        System.out.println("好きな食べ物は□□です。");
+}
+}
