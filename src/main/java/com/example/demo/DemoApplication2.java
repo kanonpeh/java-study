ff27b83b609f7bae586c321d8bbbd4bb40137862
kanon <kanonpeh@gmail,com>
2025-10-10 00:54:04 +0900

    add DemoApplication2 class with example of name, score, and assessment in Japanese

---
+++ src/main/java/com/example/demo/DemoApplication2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+public class DemoApplication2 {
+    public static void main(String[] args) {
+        String name ="河合彩香";//名前
+        float value = 62.7f;//偏差値
+        char assess = 'A';//評価
+
+        //文字列の連結（連結演算子を使用）
+        String result = name + "さんの偏差値は" + value + "で、評価は" + assess + "です。";
+        System.out.println(result);
+    }
+}
