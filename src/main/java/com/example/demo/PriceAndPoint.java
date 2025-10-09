f8bca2d22b0de13da4f86ffedc88e706a002591b
kanon <kanonpeh@gmail,com>
2025-10-09 23:43:08 +0900

    add PriceAndPoint and Time classes with examples in Japanese

---
+++ src/main/java/com/example/demo/PriceAndPoint.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class PriceAndPoint {
+    public static void main(String[] args) {
+
+        int price =300;        //変数の宣言・代入
+
+        int point; //変数の宣言
+        point =9; //変数への値の代入
+
+        //表示
+        System.out.println("金額："+ +price +"円");
+        System.out.println("取得したポイント" + point+"ポイント" );
+    }
+}
---
+++ src/main/java/com/example/demo/Time.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+public class Time {
+    public static void main(String[] args) {
+        //定数の宣言・初期化
+        final int START_TIME = 9;        //開始時刻
+
+        //変数の宣言・初期化
+        int correntTime  =7;          //現在時刻
+
+        //成績情報の表示
+        System.out.println("開始時刻" +START_TIME + "時");
+        System.out.println("現在時刻" +correntTime + "時");
+    }
+}
