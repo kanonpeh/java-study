0f38875df64b4cfdaf42f674c291179575cc420c
kanon <kanonpeh@gmail,com>
2025-10-11 01:19:13 +0900

    add DemoApplication3 class with self-introduction example in Japanese

---
+++ src/main/java/com/example/demo/DemoApplication3.java
@@ -0,0 +1,16 @@
+package com.example.demo;
+
+public class DemoApplication3 {
+    public static void main(String[] args) {
+       //変数の宣言・初期化
+        String name = "速水剛瑠"; //名前
+        int month = 8;          //月
+        int day = 21;          //日
+
+        //文字列連結
+        String myINtro =  name + "です。誕生日は" + month + "月" + day + "日です。";
+        System.out.println(myINtro);
+
+
+    }
+}
