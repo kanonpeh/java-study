6cf35b7a8ae187a3d5bd95159cd6936657b8f7f4
kanon <kanonpeh@gmail,com>
2025-10-11 01:41:14 +0900

    add DemoApplication4 class with arithmetic operations examples in Japanese

---
+++ src/main/java/com/example/demo/DemoApplication4.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+public class DemoApplication4 {
+    public static void main(String[] args) {
+        //変数の宣言・初期化
+        int a = 5;
+        int b = 2;
+
+        //加算
+        int result1  = a + b;
+        System.out.println("result1: " + result1);
+
+        //減算
+        int result2 = a - b;
+        System.out.println("result2: " + result2);
+
+        //乗算
+        int result3 = a * b;
+        System.out.println("result3: " + result3);
+
+        //除算
+        int result4 = a / b;
+        System.out.println("result4: " + result4);
+
+        //剰余
+        int result5 = a % b;
+        System.out.println("result5: " + result5);
+    }
+}
